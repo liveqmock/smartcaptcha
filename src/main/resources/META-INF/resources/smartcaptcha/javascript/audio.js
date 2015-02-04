ffedff1a8e058a2d8d97d3dbb9d5a0e7fab2dbf4
rustock0 <rustock@lna.org.ru>
2015-02-04 20:12:33 +0300

    Audio was fixed for Mozilla Firefox

--- src/main/resources/META-INF/resources/smartcaptcha/javascript/audio.js
+++ src/main/resources/META-INF/resources/smartcaptcha/javascript/audio.js
@@ -1,7 +1,9 @@
 playSound = function(soundUrl) {
 
     if (checkHtml5AudioSupport()) {
-        var sound = new Audio(soundUrl);
+        // Because of Mozilla Firefox bug we should to generate random parameter of request the order to guarantee invocation of servlet.
+        // See more: https://bugzilla.mozilla.org/show_bug.cgi?id=1129121
+        var sound = new Audio(soundUrl + "?" + Math.random());
         sound.type = 'audio/wav';
         sound.autobuffer = false;
 
